540a9869e94dfd2875baec64049719bff0aaaf10
MatheusFr27 <matheusefranca1727@gmail.com>
2021-03-24 16:09:47 -0300

    Adicionando propriedade url

--- store/Documentation.js
+++ store/Documentation.js
@@ -2,9 +2,12 @@ export const state = () => ({
   Criadores: '@Karlla,@Ana,@Matheus,@Debora',
   Nome_do_site: 'Nome do site',
   Acessibilidade: [
-    { text: 'Atalhos de acessibilidade' },
-    { text: 'Mapa do site' },
+    { text: 'Atalhos de acessibilidade', url: '' },
+    { text: 'Mapa do site', url: '' },
   ],
-  Fac: [{ text: 'Precisa de ajuda?' }, { text: 'Enviar "freedback"' }],
+  Fac: [
+    { text: 'Precisa de ajuda?', url: '' },
+    { text: 'Enviar "freedback"', url: '' },
+  ],
   Network: [{ text: 'Facebook' }, { text: 'Telegram' }],
 })
